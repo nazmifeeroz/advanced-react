79210130845c15e49738e627bbaff453bbe46be7
Nazmi <nazmifeeroz@hotmail.com>
2019-06-10 22:37:29 +0800

    themes and layouts with styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 import Header from '../components/Header'
 import Meta from '../components/Meta'
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0,0,0,0.9)',
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`
+
 const Page = ({ children }) => {
   return (
-    <div>
-      <Meta />
-      <Header />
-      {children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <Inner>{children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   )
 }
 
