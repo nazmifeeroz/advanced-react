b0657808c1b89fca6899fb6f0d3e87df541a8c06
Nazmi <nazmifeeroz@hotmail.com>
2019-06-13 07:39:44 +0800

    Items creation and prisma yoga flow

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,7 +1,6 @@
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    return global.dogs
+  items(parent, args, ctx, info) {
+    return ctx.db.query.items()
   },
 }
 
